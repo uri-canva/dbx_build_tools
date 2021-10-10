919a9c5e19bad431cfaea8a8e90e3dc7c0adf920
Uri Baghin <uri@canva.com>
2021-10-10 13:49:25 +1100

    Fix go_binary path.

--- build_tools/services/restart_test.sh
+++ build_tools/services/restart_test.sh
@@ -4,8 +4,8 @@ set -eux
 # Use a var to avoid a pipe - that would make number of processes nondeterministic
 out=$(ps ux)
 procs1=$(wc -l <<< out)
-$RUNFILES/../dbx_build_tools/build_tools/svcctl/cmd/svcctl/svcctl stop-all
-$RUNFILES/../dbx_build_tools/build_tools/svcctl/cmd/svcctl/svcctl start-all
+$RUNFILES/../dbx_build_tools/build_tools/svcctl/cmd/svcctl/svcctl_/svcctl stop-all
+$RUNFILES/../dbx_build_tools/build_tools/svcctl/cmd/svcctl/svcctl_/svcctl start-all
 out=$(ps ux)
 procs2=$(wc -l <<< out)
 if [ "$procs1" -lt "$procs2" ]; then
